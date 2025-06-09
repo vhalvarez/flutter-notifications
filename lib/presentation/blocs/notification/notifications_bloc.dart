f0a6762f338c52736627da29ba65c4c134f0152d
Victor Alvarez <alvarez3197@gmail.com>
2025-06-09 11:42:30 -0400

    token del dispositivo y determinar permiso actual

--- lib/presentation/blocs/notification/notifications_bloc.dart
+++ lib/presentation/blocs/notification/notifications_bloc.dart
@@ -14,6 +14,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   NotificationsBloc() : super(const NotificationsState()) {
     on<NotificationStatusChanged>(_notificationStatusChanged);
+    _initialStatusCheck();
   }
 
   static Future<void> initializeFCM() async {
@@ -27,6 +28,22 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     Emitter<NotificationsState> emit,
   ) {
     emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
+  void _getFCMToken() async {
+   final settings = await messaging.getNotificationSettings();
+
+   if(state.status != AuthorizationStatus.authorized) return;
+
+   final token = await messaging.getToken();
+
+   print(token);
   }
 
   void requestPermission() async {
