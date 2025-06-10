da1e3c3e57fc2bdf971d4bea7494321a129352da
Victor Alvarez <alvarez3197@gmail.com>
2025-06-10 11:28:28 -0400

    notificaciones cuando la app esta terminada, entiedad para el manejo de notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:push_app/config/router/app_router.dart';
@@ -6,7 +7,9 @@ import 'package:push_app/presentation/blocs/notification/notifications_bloc.dart
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   await NotificationsBloc.initializeFCM();
+
   
   runApp(
     MultiBlocProvider(
--- lib/presentation/blocs/notification/notifications_bloc.dart
+++ lib/presentation/blocs/notification/notifications_bloc.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:push_app/domain/entities/push_message.dart';
 import 'package:push_app/firebase_options.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -9,12 +12,25 @@ import 'package:equatable/equatable.dart';
 part 'notifications_event.dart';
 part 'notifications_state.dart';
 
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsState()) {
     on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    // Verificar estado de las notificaciones
     _initialStatusCheck();
+
+    //
+    _onForegroundMessage();
   }
 
   static Future<void> initializeFCM() async {
@@ -37,13 +53,38 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   void _getFCMToken() async {
-   final settings = await messaging.getNotificationSettings();
+    final settings = await messaging.getNotificationSettings();
 
-   if(state.status != AuthorizationStatus.authorized) return;
+    if (state.status != AuthorizationStatus.authorized) return;
 
-   final token = await messaging.getToken();
+    final token = await messaging.getToken();
 
-   print(token);
+    print(token);
+  }
+
+  void _handleRemoteMessage(RemoteMessage message) {
+    if (message.notification == null) return;
+
+    final notification = PushMessage(
+      messageId:
+          message.messageId?.replaceAll(':', '').replaceAll('%', '') ?? '',
+      title: message.notification?.title ?? '',
+      body: message.notification?.body ?? '',
+      sentDate: DateTime.now(),
+      data: message.data,
+      imageUrl:
+          Platform.isAndroid
+              ? message.notification?.android?.imageUrl
+              : message.notification?.apple?.imageUrl,
+    );
+
+    // Add the notification to the state
+    // emit(state.copyWith(notifications: [...state.notifications, notification]));
+    print(notification);
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
   }
 
   void requestPermission() async {
